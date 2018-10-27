00b17c5249259ab9771ac7e263beb977fabcb57c
Jonathan Bryant <jonathan@theshire.space>
2018-10-27 05:09:23 +0000

    Build Fixes to build entrypoint.

--- images/build/build.sh
+++ images/build/build.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
+FILELS=$(ls /manifests/*-atomic-host.json)
+FILE=${FILELS[0]}
+
 ostree --repo=/srv/repo init --mode=archive-z2
-rpm-ostree compose tree  --cachedir=/srv  --repo=/srv/repo /manifests/fedora-atomic-host.json
+rpm-ostree compose tree  --cachedir=/srv  --repo=/srv/repo /manifests/$FILE
